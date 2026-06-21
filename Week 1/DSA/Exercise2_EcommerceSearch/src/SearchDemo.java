42fc8495f7f5b1adf408d17a7416c0dd5bc25af7
Gayathri Thoravi <gayathritoravi@gmail.com>
2026-06-21 22:38:17 +0530

    Completed DSA Exercise 2 Ecommerce Platform Search Function

---
+++ Week 1/DSA/Exercise2_EcommerceSearch/src/SearchDemo.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SearchDemo {
+
+    
+    public static Product linearSearch(Product[] products, String name) {
+
+        for (Product p : products) {
+            if (p.productName.equalsIgnoreCase(name)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+
+    public static Product binarySearch(Product[] products, String name) {
+
+        int left = 0;
+        int right = products.length - 1;
+
+        while (left <= right) {
+
+            int mid = (left + right) / 2;
+
+            int result =
+                    products[mid].productName.compareToIgnoreCase(name);
+
+            if (result == 0)
+                return products[mid];
+
+            if (result < 0)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        Product[] products = {
+                new Product(101, "Laptop", "Electronics"),
+                new Product(102, "Mobile", "Electronics"),
+                new Product(103, "Shoes", "Fashion"),
+                new Product(104, "Watch", "Accessories")
+        };
+
+        Product linearResult =
+                linearSearch(products, "Shoes");
+
+        if (linearResult != null) {
+            System.out.println("Linear Search Found: "
+                    + linearResult.productName);
+        }
+
+        Arrays.sort(products,
+                Comparator.comparing(p -> p.productName));
+
+        Product binaryResult =
+                binarySearch(products, "Shoes");
+
+        if (binaryResult != null) {
+            System.out.println("Binary Search Found: "
+                    + binaryResult.productName);
+        }
+    }
+}
