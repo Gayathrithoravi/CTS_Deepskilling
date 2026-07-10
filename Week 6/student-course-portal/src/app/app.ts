de46e6c020d0c5fe3b86b8392c6c97dcb6f78b80
Gayathri Thoravi <gayathritoravi@gmail.com>
2026-07-10 10:02:17 +0530

    Completed Angular Hands-On 4 - Template Driven Forms

--- Week 6/student-course-portal/src/app/app.ts
+++ Week 6/student-course-portal/src/app/app.ts
@@ -3,17 +3,17 @@ import { RouterOutlet } from '@angular/router';
 
 import { Header } from './components/header/header';
 import { Home } from './pages/home/home';
+import { EnrollmentForm } from './pages/enrollment-form/enrollment-form';
 import { CourseList } from './pages/course-list/course-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    Header,
-    Home,
-    CourseList
-  ],
+  RouterOutlet,
+  Header,
+  EnrollmentForm
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
